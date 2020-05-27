475a3fd7672482dce700806384736814a5743387
Michael Perez <mperez6473@gmail.com>
2020-05-27 10:56:45 -0500

    token is recieved from Alex's login form

--- dev-queue/src/App.js
+++ dev-queue/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Switch, Route } from "react-router-dom";
 import './App.css';
 import Dashboard from './components/dev/Dashboard';
 import Login from './components/dev/Login';
@@ -12,32 +11,26 @@ import Header from './components/header';
 import RegisterForm from './components/register';
 import LoginForm from './components/login';
 
-
 function App() {
     return (
         <>
             <Header />
             <Switch>
-
                 // Dashboard
                 <Route exact path="/">
                     <Dashboard />
                 </Route>
-      
                 <Route path="/create">
-                  <AddTicket />
+                    <AddTicket />
                 </Route>
-                
                 // Login page
                 <Route path="/login">
                     <LoginForm />
                 </Route>
-
                 // Register page
                 <Route path="/register">
                     <RegisterForm />
                 </Route>
-
             </Switch>
         </>
     );
--- dev-queue/src/components/login/index.js
+++ dev-queue/src/components/login/index.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import * as yup from 'yup';
 import axios from 'axios';
 
 import './Login.css';
-import { Form, FormGroup, Input, Label, Button, UncontrolledAlert } from 'reactstrap';
-import loginFormSchema from "./loginFormSchema";
-
+import {
+    Form,
+    FormGroup,
+    Input,
+    Label,
+    Button,
+    UncontrolledAlert,
+} from 'reactstrap';
+import loginFormSchema from './loginFormSchema';
 
 export default function LoginForm(props) {
     const initialErrors = {
         email: '',
-        password: ''
+        password: '',
     };
 
     const initialFormData = {
         email: '',
-        password: ''
+        password: '',
     };
 
     const LOGIN_URL = 'https://bwdevdesk3.herokuapp.com/auth/login';
@@ -26,81 +32,90 @@ export default function LoginForm(props) {
     let [disabled, setDisabled] = useState(true);
 
     function updateFormData(key, value) {
-        setFormData({...formData, [key]: value});
+        setFormData({ ...formData, [key]: value });
     }
 
-    function inputOnChangeHandler (event) {
-
+    function inputOnChangeHandler(event) {
         const element = event.target;
-
 
         updateFormData(element.name, element.value);
 
         yup.reach(loginFormSchema, element.name)
             .validate(formData[element.name])
-            .then(valid => {
-                setErrors({...errors, [element.name]: ''});
+            .then((valid) => {
+                setErrors({ ...errors, [element.name]: '' });
             })
-            .catch(err => {
-                setErrors({...errors, [element.name]: err.errors[0]});
+            .catch((err) => {
+                setErrors({ ...errors, [element.name]: err.errors[0] });
             });
-    
     }
 
     useEffect(() => {
-        loginFormSchema.isValid(formData)
-            .then((valid) => {
-                if (valid) {
-                    setDisabled(false);
-                    setErrors(initialErrors);
-                } else {
-                    setDisabled(true);
-                }
-            });
+        loginFormSchema.isValid(formData).then((valid) => {
+            if (valid) {
+                setDisabled(false);
+                setErrors(initialErrors);
+            } else {
+                setDisabled(true);
+            }
+        });
     }, [formData]);
 
     function onSubmitHandler(event) {
         event.preventDefault();
 
-        axios.post(LOGIN_URL, formData)
+        axios
+            .post(LOGIN_URL, formData)
             .then((response) => {
                 console.log(response);
+                localStorage.setItem('token', response.data.token);
             })
             .catch((error) => console.log(error));
     }
-    
 
     return (
         <div className="login container">
-            {
-                Object.keys(errors).map((item, key) => {
-                    if (errors[item]) {
-                        return (
-                            <div className="row" key={key}>
-                                <div className="col">
-                                    <UncontrolledAlert color="danger">
-                                        {errors[item]}
-                                    </UncontrolledAlert>
-                                </div>
+            {Object.keys(errors).map((item, key) => {
+                if (errors[item]) {
+                    return (
+                        <div className="row" key={key}>
+                            <div className="col">
+                                <UncontrolledAlert color="danger">
+                                    {errors[item]}
+                                </UncontrolledAlert>
                             </div>
-                        );
-                    }
-                })
-            }
+                        </div>
+                    );
+                }
+            })}
             <div className="row">
                 <div className="col-lg-6">
                     <Form autoComplete="off">
                         <h2>Log In</h2>
                         <FormGroup>
                             <Label>E-mail:</Label>
-                            <Input type="email" name="email" onChange={inputOnChangeHandler} />
+                            <Input
+                                type="email"
+                                name="email"
+                                onChange={inputOnChangeHandler}
+                            />
                         </FormGroup>
                         <FormGroup>
                             <Label>Password:</Label>
-                            <Input type="password" name="password" onChange={inputOnChangeHandler} />
+                            <Input
+                                type="password"
+                                name="password"
+                                onChange={inputOnChangeHandler}
+                            />
                         </FormGroup>
                         <FormGroup>
-                            <Button disabled={disabled} onClick={onSubmitHandler} className="btn btn-danger">Log In</Button>
+                            <Button
+                                disabled={disabled}
+                                onClick={onSubmitHandler}
+                                className="btn btn-danger"
+                            >
+                                Log In
+                            </Button>
                         </FormGroup>
                     </Form>
                     Don't have an account? <Link to="/register">Register</Link>
