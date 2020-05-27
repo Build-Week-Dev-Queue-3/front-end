82cf96f30f10199ee53c46ed3545e7ad8067971f
Michael Perez <mperez6473@gmail.com>
2020-05-27 17:06:07 -0500

    Have an edit form sorta taking shape.

--- dev-queue/src/components/EditProfile/EditProfile.jsx
+++ dev-queue/src/components/EditProfile/EditProfile.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const EditProfile = (props) => {
     console.log('these are your props: ', props.you);
+    const [editing, setEditing] = useState(false);
+    const [profile, setProfile] = useState(props.you);
+    const editProfile = () => {
+        setEditing(!editing);
+    };
+    console.log(profile);
     return (
         <section>
             <h3>Welcome to your profile!</h3>
-            <ul>
-                <li>Name: {props.you.name} </li>
-                <li>Email: {props.you.email}</li>
-                <li>Cohort: {props.you.cohort} </li>
-                {props.you.student ? <li>Student</li> : null}
-                {props.you.helper ? <li>helper</li> : null}
-            </ul>
-            <button>Edit your profile</button>
+            {!editing ? (
+                <>
+                    <ul>
+                        <li>Name: {props.you.name} </li>
+                        <li>Email: {props.you.email}</li>
+                        <li>Cohort: {props.you.cohort} </li>
+                        <br />
+                        <h6>You are a: </h6>
+                        {props.you.student ? <li>Student</li> : null}
+                        {props.you.helper ? <li>helper</li> : null}
+                    </ul>
+                    <button onClick={editProfile}>Edit your profile</button>
+                </>
+            ) : (
+                <form>
+                    <label>
+                        Name:{' '}
+                        <input name="name" type="text" value={profile.name} />
+                    </label>{' '}
+                    <label>
+                        Email:{' '}
+                        <input name="name" type="text" value={profile.email} />
+                    </label>{' '}
+                    <button onClick={editProfile}>Cancel</button>
+                    <button>Submit</button>
+                </form>
+            )}{' '}
         </section>
     );
 };
