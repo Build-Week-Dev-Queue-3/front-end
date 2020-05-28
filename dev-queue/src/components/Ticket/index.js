04de41d8750e8a9a43809986c81f8f204dc07689
Michael Perez <mperez6473@gmail.com>
2020-05-28 09:50:37 -0500

    have the basic put request set up but the link is not complete yet

--- dev-queue/src/components/Ticket/index.js
+++ dev-queue/src/components/Ticket/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Ticket.css';
+import { authenticatedAxios } from '../../utils/authenticAxios';
 
 export default function Ticket(props) {
     const { id, subject, status, name, ticket_text, user_id } = props.queue;
@@ -7,6 +8,35 @@ export default function Ticket(props) {
     console.log('user id', user_id);
     console.log('your id', you.id);
 
+    const [editing, setEditing] = useState(false);
+    const [ticket, setTicket] = useState(props.queue);
+
+    const edit = (e) => {
+        e.preventDefault();
+        setEditing(!editing);
+    };
+    console.log(editing);
+
+    const handleChanges = (e) => {
+        e.persist();
+        setTicket({
+            ...ticket,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        authenticatedAxios()
+            .put(`/tickets/:id/user/:uid`, ticket)
+            .then((res) => {
+                console.log(res);
+                edit();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    console.log(ticket);
     return (
         <div className="row ticket">
             <div className="col">
@@ -19,17 +49,46 @@ export default function Ticket(props) {
                         <p className="ticket__status">{status}</p>
                     </div>
                     <div className="col ticket__content">
-                        <h3>{subject}</h3>
-                        <p>{ticket_text}</p>
-
-                        {you.helper || you.id === user_id ? (
-                            <div className="ticket__actions">
-                                <a href="#">Edit</a>
-                                <a href="#" className="text-danger">
-                                    Delete
-                                </a>
-                            </div>
-                        ) : null}
+                        {editing ? (
+                            <form onSubmit={handleSubmit}>
+                                <h3>
+                                    Subject:{' '}
+                                    <input
+                                        name="subject"
+                                        value={subject}
+                                        onChange={handleChanges}
+                                    />
+                                </h3>
+                                <p>
+                                    Description:{' '}
+                                    <textarea
+                                        name="ticket_text"
+                                        value={ticket_text}
+                                        onChange={handleChanges}
+                                    />
+                                </p>
+                                <button>Submit</button>
+                                <button>Cancel</button>
+                            </form>
+                        ) : (
+                            <>
+                                {' '}
+                                <h3>{subject}</h3>
+                                <p>{ticket_text}</p>
+                            </>
+                        )}
+                        {you.helper || you.id === user_id
+                            ? !editing && (
+                                  <div className="ticket__actions">
+                                      <a href="#" onClick={edit}>
+                                          Edit
+                                      </a>
+                                      <a href="#" className="text-danger">
+                                          Delete
+                                      </a>
+                                  </div>
+                              )
+                            : null}
                     </div>
                 </div>
             </div>
